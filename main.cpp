d526061989ce082f29d9ef1ba6577c38e4e4801b
Szymon Wilczek <szymonwilczek@outlook.com>
2025-03-08 18:54:31 +0100

    implementacja `wavelength` w `main`

--- main.cpp
+++ main.cpp
@@ -7,8 +7,14 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QGraphicsDropShadowEffect>
+#include <QStackedWidget>
+#include <QTimer>
 
 #include "blob/core/app_instance_manager.h"
+#include "wavelength/join_wavelength_dialog.h"
+#include "wavelength/wavelength_chat_view.h"
+#include "wavelength/wavelength_dialog.h"
+#include "wavelength/wavelength_manager.h"
 
 void centerLabel(QLabel* label, BlobAnimation* animation) {
     if (label && animation) {
@@ -79,12 +85,27 @@ int main(int argc, char *argv[]) {
     mainLayout->setContentsMargins(0, 0, 0, 0);
     mainLayout->setSpacing(0);
 
-    auto *animation = new BlobAnimation(centralWidget);
+    QStackedWidget *stackedWidget = new QStackedWidget(centralWidget);
+    mainLayout->addWidget(stackedWidget);
+
+    QWidget *animationWidget = new QWidget(stackedWidget);
+    QVBoxLayout *animationLayout = new QVBoxLayout(animationWidget);
+    animationLayout->setContentsMargins(0, 0, 0, 0);
+    animationLayout->setSpacing(0);
+
+    auto *animation = new BlobAnimation(animationWidget);
     animation->setBackgroundColor(QColor(35, 35, 35));
     animation->setGridColor(QColor(60, 60, 60));
     animation->setGridSpacing(20);
+    animationLayout->addWidget(animation);
 
-    mainLayout->addWidget(animation);
+    stackedWidget->addWidget(animationWidget);
+
+    WavelengthChatView *chatView = new WavelengthChatView(stackedWidget);
+    stackedWidget->addWidget(chatView);
+
+    stackedWidget->setCurrentWidget(animationWidget);
+
     window.setCentralWidget(centralWidget);
 
     QLabel *titleLabel = new QLabel("Hello, World!", animation);
@@ -128,14 +149,12 @@ int main(int argc, char *argv[]) {
 
     QObject::connect(instanceManager, &AppInstanceManager::absorptionStarted, [animation, instanceManager](const QString& targetId) {
         if (targetId == instanceManager->getInstanceId()) {
-            // Ta instancja jest celem absorpcji
             animation->startBeingAbsorbed();
         }
     });
 
     QObject::connect(instanceManager, &AppInstanceManager::instanceAbsorbed, [animation, instanceManager](const QString& targetId) {
         if (targetId == instanceManager->getInstanceId()) {
-            // Ta instancja bedzie pochlonieta
             animation->finishBeingAbsorbed();
         }
     });
@@ -143,6 +162,84 @@ int main(int argc, char *argv[]) {
     QObject::connect(instanceManager, &AppInstanceManager::absorptionCancelled, [animation, instanceManager](const QString& targetId) {
         if (targetId == instanceManager->getInstanceId()) {
             animation->cancelAbsorption();
+        }
+    });
+
+    WavelengthManager* wavelengthManager = WavelengthManager::getInstance();
+
+    QObject::connect(&app, &QApplication::aboutToQuit, [wavelengthManager]() {
+        int activeFreq = wavelengthManager->getActiveWavelength();
+        if (activeFreq > 0) {
+            wavelengthManager->closeWavelength(activeFreq);
+            qDebug() << "Closing wavelength server on application exit:" << activeFreq;
+        }
+    });
+
+    QObject::connect(navbar, &Navbar::createWavelengthClicked, [&window, animation, chatView, stackedWidget]() {
+        qDebug() << "Create wavelength button clicked";
+
+        animation->setLifeColor(QColor(0, 200, 0));
+
+        WavelengthDialog dialog(&window);
+        if (dialog.exec() == QDialog::Accepted) {
+            int frequency = dialog.getFrequency();
+            QString name = dialog.getName();
+            bool isPasswordProtected = dialog.isPasswordProtected();
+            QString password = dialog.getPassword();
+
+            WavelengthManager* manager = WavelengthManager::getInstance();
+            if (manager->createWavelength(frequency, name, isPasswordProtected, password)) {
+                chatView->setWavelength(frequency, name);
+                stackedWidget->setCurrentWidget(chatView);
+                chatView->show();
+                qDebug() << "Created and joined wavelength:" << frequency << "Hz";
+            } else {
+                qDebug() << "Failed to create wavelength";
+                animation->resetLifeColor();
+            }
+        } else {
+            animation->resetLifeColor();
+        }
+    });
+
+    QObject::connect(navbar, &Navbar::joinWavelengthClicked, [&window, chatView, stackedWidget]() {
+        qDebug() << "Join wavelength button clicked";
+
+        JoinWavelengthDialog dialog(&window);
+        if (dialog.exec() == QDialog::Accepted) {
+            int frequency = dialog.getFrequency();
+            QString password = dialog.getPassword();
+
+            WavelengthManager* manager = WavelengthManager::getInstance();
+            if (!manager->isFrequencyAvailable(frequency)) {
+                if (manager->joinWavelength(frequency, password)) {
+                    chatView->setWavelength(frequency);
+                    stackedWidget->setCurrentWidget(chatView);
+                    chatView->show();
+                    qDebug() << "Joined wavelength:" << frequency << "Hz";
+                } else {
+                    qDebug() << "Failed to join wavelength";
+                }
+            } else {
+                qDebug() << "Wavelength does not exist on frequency:" << frequency;
+            }
+        }
+    });
+
+    QObject::connect(chatView, &WavelengthChatView::wavelengthAborted, [stackedWidget, animationWidget, animation]() {
+        qDebug() << "Wavelength aborted, returning to main view";
+        stackedWidget->setCurrentWidget(animationWidget);
+        animation->resetLifeColor();
+    });
+
+    QObject::connect(wavelengthManager, &WavelengthManager::wavelengthClosed, [stackedWidget, animationWidget, animation, chatView, wavelengthManager](int frequency) {
+        if (wavelengthManager->getActiveWavelength() == frequency) {
+            qDebug() << "Current wavelength was closed by host, returning to main view";
+            stackedWidget->setCurrentWidget(animationWidget);
+            animation->resetLifeColor();
+            QTimer::singleShot(100, [chatView]() {
+                chatView->hide();
+            });
         }
     });
 
