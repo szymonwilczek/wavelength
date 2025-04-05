e74bf0332915444e7689f5cdbe9715865ab266c9
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-04-05 14:18:10 +0200

    new implementation of `font manager`

--- font_manager.cpp
+++ font_manager.cpp
@@ -1,163 +1,100 @@
 #include "font_manager.h"
-#include <QApplication>
+#include <QFontDatabase>
 #include <QDebug>
+#include <QDirIterator>
+#include <QCoreApplication>
 
-FontManager& FontManager::instance() {
-    static FontManager instance;
+FontManager* FontManager::instance = nullptr;
+
+FontManager* FontManager::getInstance() {
+    if (instance == nullptr) {
+        instance = new FontManager();
+    }
     return instance;
 }
 
-FontManager::FontManager() {
-    m_fontFamilyNames = {
-        {FontFamily::Lato, "Lato"},
-        {FontFamily::NotoSans, "Noto Sans Condensed"},
-        {FontFamily::Poppins, "Poppins"},
-        {FontFamily::BlenderPro, "Blender Pro"}
-    };
-
-    m_fontStyleNames = {
-        {FontStyle::Regular, "Regular"},
-        {FontStyle::Bold, "Bold"},
-        {FontStyle::Italic, "Italic"},
-        {FontStyle::Light, "Light"},
-        {FontStyle::Black, "Black"},
-        {FontStyle::Thin, "Thin"},
-        {FontStyle::BoldItalic, "Bold Italic"},
-        {FontStyle::BlackItalic, "Black Italic"},
-        {FontStyle::LightItalic, "Light Italic"},
-        {FontStyle::ThinItalic, "Thin Italic"},
-        {FontStyle::Medium, "Medium"},
-        {FontStyle::SemiBold, "SemiBold"}
-    };
-
-    m_fontPaths = {
-        {qMakePair(FontFamily::Lato, FontStyle::Regular), ":/assets/fonts/Lato/Lato-Regular.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::Bold), ":/assets/fonts/Lato/Lato-Bold.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::Italic), ":/assets/fonts/Lato/Lato-Italic.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::Light), ":/assets/fonts/Lato/Lato-Light.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::Black), ":/assets/fonts/Lato/Lato-Black.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::Thin), ":/assets/fonts/Lato/Lato-Thin.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::BoldItalic), ":/assets/fonts/Lato/Lato-BoldItalic.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::BlackItalic), ":/assets/fonts/Lato/Lato-BlackItalic.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::LightItalic), ":/assets/fonts/Lato/Lato-LightItalic.ttf"},
-        {qMakePair(FontFamily::Lato, FontStyle::ThinItalic), ":/assets/fonts/Lato/Lato-ThinItalic.ttf"},
-
-        {qMakePair(FontFamily::NotoSans, FontStyle::Regular), ":/assets/fonts/Noto_Sans/NotoSans_Condensed-Regular.ttf"},
-        {qMakePair(FontFamily::NotoSans, FontStyle::Bold), ":/assets/fonts/Noto_Sans/NotoSans_Condensed-Bold.ttf"},
-        {qMakePair(FontFamily::NotoSans, FontStyle::Black), ":/assets/fonts/Noto_Sans/NotoSans_Condensed-Black.ttf"},
-        {qMakePair(FontFamily::NotoSans, FontStyle::Light), ":/assets/fonts/Noto_Sans/NotoSans_Condensed-Light.ttf"},
-        {qMakePair(FontFamily::NotoSans, FontStyle::Medium), ":/assets/fonts/Noto_Sans/NotoSans_Condensed-Medium.ttf"},
-        {qMakePair(FontFamily::NotoSans, FontStyle::SemiBold), ":/assets/fonts/Noto_Sans/NotoSans_Condensed-SemiBold.ttf"},
-
-        {qMakePair(FontFamily::Poppins, FontStyle::Regular), ":/assets/fonts/Poppins/Poppins-Regular.ttf"},
-        {qMakePair(FontFamily::Poppins, FontStyle::Bold), ":/assets/fonts/Poppins/Poppins-Bold.ttf"},
-        {qMakePair(FontFamily::Poppins, FontStyle::Medium), ":/assets/fonts/Poppins/Poppins-Medium.ttf"},
-        {qMakePair(FontFamily::Poppins, FontStyle::Light), ":/assets/fonts/Poppins/Poppins-Light.ttf"},
-        {qMakePair(FontFamily::Poppins, FontStyle::SemiBold), ":/assets/fonts/Poppins/Poppins-SemiBold.ttf"},
-
-        {qMakePair(FontFamily::BlenderPro, FontStyle::Regular), ":/assets/fonts/BlenderPro/BlenderPro-Medium.ttf"},
-        {qMakePair(FontFamily::BlenderPro, FontStyle::Bold), ":/assets/fonts/BlenderPro/BlenderPro-Bold.ttf"},
-        {qMakePair(FontFamily::BlenderPro, FontStyle::Italic), ":/assets/fonts/BlenderPro/BlenderPro-BoldItalic.ttf"},
-
-
-    };
-
-    m_loadedFonts[FontFamily::Lato] = false;
-    m_loadedFonts[FontFamily::NotoSans] = false;
-    m_loadedFonts[FontFamily::Poppins] = false;
+FontManager::FontManager(QObject* parent) : QObject(parent) {
 }
 
 bool FontManager::initialize() {
-    bool allLoaded = true;
+    bool success = true;
     
-    for (auto it = m_fontPaths.begin(); it != m_fontPaths.end(); ++it) {
-        FontFamily family = it.key().first;
-        if (!loadFont(it.value())) {
-            qWarning() << "Error loading font: " << it.value();
-            allLoaded = false;
-        } else {
-            m_loadedFonts[family] = true;
+    // Podstawowe ścieżki do sprawdzenia
+    QStringList basePaths = {
+        ":/assets/fonts/",                                   // ścieżka do czcionek w zasobach
+        QCoreApplication::applicationDirPath() + "/../assets/fonts/",  // względna ścieżka dla trybu release
+        QCoreApplication::applicationDirPath() + "/../../assets/fonts/" // względna ścieżka dla trybu debug
+    };
+    
+    // Lista czcionek do załadowania z ich katalogami
+    QMap<QString, QString> fontDirs = {
+        {"BlenderPro", "BlenderPro"},
+        {"Lato", "Lato"},
+        {"Poppins", "Poppins"}
+    };
+    
+    for (const auto& fontName : fontDirs.keys()) {
+        bool fontLoaded = false;
+        QString dirName = fontDirs[fontName];
+        
+        for (const QString& basePath : basePaths) {
+            QString fontDir = basePath + dirName + "/";
+            QDirIterator it(fontDir, QStringList() << "*.ttf", QDir::Files);
+            
+            while (it.hasNext()) {
+                QString fontPath = it.next();
+                if (loadFont(fontName, fontPath)) {
+                    qDebug() << "Załadowano czcionkę:" << fontPath;
+                    fontLoaded = true;
+                    // Nie przerywamy, żeby załadować wszystkie warianty (.ttf) danej czcionki
+                }
+            }
+            
+            if (fontLoaded) {
+                break; // Jeśli znaleźliśmy i załadowaliśmy czcionkę w tej ścieżce, nie sprawdzamy pozostałych
+            }
+        }
+        
+        if (!fontLoaded) {
+            qWarning() << "Nie udało się załadować czcionki:" << fontName;
+            success = false;
         }
     }
     
-    if (allLoaded) {
-        qDebug() << "All fonts loaded successfully";
-    } else {
-        qWarning() << "Some fonts failed to load";
-    }
-    
-    return allLoaded;
+    return success;
 }
 
-bool FontManager::loadFont(const QString& path) {
-    int fontId = QFontDatabase::addApplicationFont(path);
-    return fontId != -1;
-}
-
-QFont FontManager::getFont(FontFamily family, FontStyle style, int pointSize) {
-    QString familyName = m_fontFamilyNames.value(family, "Lato");
-    QString styleName = m_fontStyleNames.value(style, "Regular");
-    
-    QFont font(familyName, pointSize);
-    
-    switch (style) {
-        case FontStyle::Bold:
-        case FontStyle::BoldItalic:
-            font.setWeight(QFont::Bold);
-            break;
-        case FontStyle::Light:
-        case FontStyle::LightItalic:
-            font.setWeight(QFont::Light);
-            break;
-        case FontStyle::Black:
-        case FontStyle::BlackItalic:
-            font.setWeight(QFont::Black);
-            break;
-        case FontStyle::Thin:
-        case FontStyle::ThinItalic:
-            font.setWeight(QFont::Thin);
-            break;
-        case FontStyle::Medium:
-            font.setWeight(QFont::Medium);
-            break;
-        case FontStyle::SemiBold:
-            font.setWeight(QFont::DemiBold);
-            break;
-        default:
-            font.setWeight(QFont::Normal);
-            break;
+bool FontManager::loadFont(const QString& fontName, const QString& fontPath) {
+    int fontId = QFontDatabase::addApplicationFont(fontPath);
+    if (fontId == -1) {
+        qWarning() << "Błąd ładowania czcionki:" << fontPath;
+        return false;
     }
     
-    font.setItalic(style == FontStyle::Italic ||
-                  style == FontStyle::BoldItalic || 
-                  style == FontStyle::LightItalic || 
-                  style == FontStyle::BlackItalic || 
-                  style == FontStyle::ThinItalic);
+    QStringList families = QFontDatabase::applicationFontFamilies(fontId);
+    if (families.isEmpty()) {
+        qWarning() << "Brak dostępnych rodzin czcionek dla:" << fontPath;
+        return false;
+    }
     
-    font.setStyleStrategy(QFont::PreferAntialias);
+    QString family = families.first();
+    fontFamilies[fontName] = family;
+    qDebug() << "Załadowana rodzina czcionki:" << fontName << "jako" << family;
+    return true;
+}
+
+QString FontManager::getFontFamily(const QString& fontName) {
+    if (fontFamilies.contains(fontName)) {
+        return fontFamilies[fontName];
+    }
     
+    qWarning() << "Nie znaleziono rodziny czcionki:" << fontName << "- używam domyślnej";
+    return QFont().family(); // Zwróć domyślną czcionkę systemową
+}
+
+QFont FontManager::getFont(const QString& fontName, int pointSize, int weight) {
+    QString family = getFontFamily(fontName);
+    QFont font(family, pointSize);
+    font.setWeight(weight);
     return font;
-}
-
-void FontManager::setApplicationFont(FontFamily family, FontStyle style, int pointSize) {
-    QFont defaultFont = getFont(family, style, pointSize);
-    QApplication::setFont(defaultFont);
-    qDebug() << "Set default font to:"
-             << m_fontFamilyNames[family] << m_fontStyleNames[style] 
-             << "with size " << pointSize;
-}
-
-bool FontManager::isFontLoaded(FontFamily family) const {
-    return m_loadedFonts.value(family, false);
-}
-
-void FontManager::debugFontInfo() const {
-    qDebug() << "== FONTS INFO ==";
-    qDebug() << "Loaded fonts families:";
-    
-    for (auto it = m_loadedFonts.begin(); it != m_loadedFonts.end(); ++it) {
-        QString familyName = m_fontFamilyNames.value(it.key());
-        bool loaded = it.value();
-        qDebug() << "  - " << familyName << ":" << (loaded ? "LOADED" : "FAILED TO LOAD");
-    }
 }
--- font_manager.h
+++ font_manager.h
@@ -1,63 +1,37 @@
 #ifndef FONT_MANAGER_H
 #define FONT_MANAGER_H
 
-#include <QFont>
-#include <QFontDatabase>
+#include <QObject>
 #include <QMap>
 #include <QString>
+#include <QFont>
 
-enum class FontFamily {
-    Lato,
-    NotoSans,
-    Poppins,
-    BlenderPro
-};
+class FontManager : public QObject {
+    Q_OBJECT
 
-enum class FontStyle {
-    Regular,
-    Bold,
-    Italic,
-    Light,
-    Black,
-    Thin,
-    BoldItalic,
-    BlackItalic,
-    LightItalic,
-    ThinItalic,
-    Medium,
-    SemiBold
-};
-
-class FontManager {
 public:
-    static FontManager& instance();
-    
+    static FontManager* getInstance();
+
+    // Inicjalizacja i ładowanie wszystkich czcionek
     bool initialize();
+
+    // Pobieranie nazwy rodziny czcionki dla danego stylu
+    QString getFontFamily(const QString& fontName);
     
-    QFont getFont(FontFamily family = FontFamily::Lato,
-                  FontStyle style = FontStyle::Regular, 
-                  int pointSize = 10);
-                  
-    void setApplicationFont(FontFamily family = FontFamily::Lato,
-                           FontStyle style = FontStyle::Regular, 
-                           int pointSize = 10);
-                           
-    bool isFontLoaded(FontFamily family) const;
-    
-    void debugFontInfo() const;
+    // Pobieranie czcionki z daną rodziną, rozmiarem i wagą
+    QFont getFont(const QString& fontName, int pointSize = 10, int weight = QFont::Normal);
 
 private:
-    FontManager();
-    
-    bool loadFont(const QString& path);
-    
-    QMap<FontFamily, QString> m_fontFamilyNames;
-    
-    QMap<FontStyle, QString> m_fontStyleNames;
-    
-    QMap<QPair<FontFamily, FontStyle>, QString> m_fontPaths;
-    
-    QMap<FontFamily, bool> m_loadedFonts;
+    FontManager(QObject* parent = nullptr);
+    ~FontManager() = default;
+
+    static FontManager* instance;
+
+    // Mapa przechowująca nazwy załadowanych rodzin czcionek
+    QMap<QString, QString> fontFamilies;
+
+    // Funkcja ładująca pojedynczą czcionkę
+    bool loadFont(const QString& fontName, const QString& fontPath);
 };
 
 #endif // FONT_MANAGER_H
