0fb9575067d30fe8b80082d89b4ede670dd7ac13
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-04-28 22:25:53 +0200

    excluded `attachment_data_store` class

---
+++ src/chat/files/attachments/attachment_data_store.h
@@ -0,0 +1,37 @@
+//
+// Created by szymo on 28.04.2025.
+//
+
+#ifndef ATTACHMENT_DATA_STORE_H
+#define ATTACHMENT_DATA_STORE_H
+#include <QMap>
+#include <qmutex.h>
+#include <QString>
+#include <QUuid>
+
+
+class AttachmentDataStore {
+public:
+    static AttachmentDataStore* getInstance() {
+        static AttachmentDataStore instance;
+        return &instance;
+    }
+
+    // Dodaj dane załącznika i zwróć identyfikator
+    QString storeAttachmentData(const QString& base64Data);
+
+    // Pobierz dane załącznika
+    QString getAttachmentData(const QString& attachmentId);
+
+    // Usuń dane załącznika (do zwalniania pamięci)
+    void removeAttachmentData(const QString& attachmentId);
+
+private:
+    AttachmentDataStore() = default;
+    QMap<QString, QString> m_attachmentData;
+    QMutex m_mutex;
+};
+
+
+
+#endif //ATTACHMENT_DATA_STORE_H
