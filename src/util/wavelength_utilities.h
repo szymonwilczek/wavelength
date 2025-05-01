83cd5d871112c23a64e57c9fb16a58416c873730
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-05-01 23:27:56 +0200

    new `util` functions for `main` class

--- src/util/wavelength_utilities.h
+++ src/util/wavelength_utilities.h
@@ -1,9 +1,17 @@
 #ifndef WAVELENGTH_UTILITIES_H
 #define WAVELENGTH_UTILITIES_H
+#include <QLabel>
+
+
+class BlobAnimation;
 
 class WavelengthUtilities {
 public:
     static double NormalizeFrequency(double frequency);
+
+    static void CenterLabel(QLabel *label, const BlobAnimation *animation);
+
+    static void UpdateTitleLabelStyle(QLabel* label, const QColor& text_color, const QColor& border_color);
 };
 
 #endif // WAVELENGTH_UTILITIES_H
