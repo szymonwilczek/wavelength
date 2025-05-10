965e0fc7bcb1a14516e62454c6f0c4b3b1776dae
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-05-10 20:33:58 +0200

    MessageFormatter code cleanup + refactoring

--- src/chat/messages/formatter/message_formatter.cpp
+++ src/chat/messages/formatter/message_formatter.cpp
@@ -1,7 +1,5 @@
 #include "message_formatter.h"
 
-#include <QJsonObject>
-
 #include "../../../app/managers/translation_manager.h"
 
 QString MessageFormatter::FormatMessage(const QJsonObject &message_object, const QString &frequency) {
@@ -24,31 +22,32 @@ QString MessageFormatter::FormatMessage(const QJsonObject &message_object, const
         }
     }
 
-    // Pobierz timestamp
     QString timestamp;
     if (message_object.contains("timestamp")) {
         QDateTime message_type;
         const QVariant timeVar = message_object["timestamp"].toVariant();
+
         if (timeVar.type() == QVariant::String) {
             message_type = QDateTime::fromString(timeVar.toString(), Qt::ISODate);
         } else {
             message_type = QDateTime::fromMSecsSinceEpoch(timeVar.toLongLong());
         }
+
         timestamp = message_type.toString("[HH:mm:ss]");
     } else {
         timestamp = QDateTime::currentDateTime().toString("[HH:mm:ss]");
     }
 
-    const TranslationManager* translator = TranslationManager::GetInstance();
+    const TranslationManager *translator = TranslationManager::GetInstance();
 
-    // Formatowanie wiadomości
     QString formatted_message;
     const bool is_self = message_object.contains("isSelf") && message_object["isSelf"].toBool();
     const bool has_attachment = message_object.contains("hasAttachment") && message_object["hasAttachment"].toBool();
 
     QString message_start;
     if (is_self) {
-        message_start = QString("%1 <span style=\"color:#60ff8a;\">[%2]:</span> ").arg(timestamp, translator->Translate("MessageFormatter.You", "You"));
+        message_start = QString("%1 <span style=\"color:#60ff8a;\">[%2]:</span> ").arg(
+            timestamp, translator->Translate("MessageFormatter.You", "You"));
     } else {
         message_start = QString("%1 <span style=\"color:#85c4ff;\">[%2]:</span> ").arg(timestamp, sender_name);
     }
@@ -58,83 +57,75 @@ QString MessageFormatter::FormatMessage(const QJsonObject &message_object, const
         QString attachment_name = message_object["attachmentName"].toString();
         QString attachment_mime_type = message_object["attachmentMimeType"].toString();
 
-        // KLUCZOWA ZMIANA: Przetwarzamy dane załącznika oddzielnie
         QString attachment_id;
 
-        // Sprawdź czy wiadomość zawiera już tylko referencję (placeholder)
-        if (!message_object["attachmentData"].toString().isEmpty() && message_object["attachmentData"].toString().length() < 100) {
-            // Używamy już przechowanego ID
+        if (!message_object["attachmentData"].toString().isEmpty() && message_object["attachmentData"].toString().
+            length() < 100) {
             attachment_id = message_object["attachmentData"].toString();
-        } else if (message_object.contains("attachmentData") && !message_object["attachmentData"].toString().isEmpty()) {
-            // Zapisz dane base64 w magazynie i uzyskaj ID
+        } else if (message_object.contains("attachmentData") && !message_object["attachmentData"].toString().
+                   isEmpty()) {
             attachment_id = AttachmentDataStore::GetInstance()->StoreAttachmentData(
                 message_object["attachmentData"].toString());
         } else {
-            // Brak danych załącznika
             attachment_id = "";
         }
 
-        // Generuj formatowanie bez umieszczania danych base64 w HTML
         if (attachment_type == "image") {
             if (attachment_mime_type == "image/gif") {
                 formatted_message = message_start + "<br>" +
-                               QString("<div class='gif-placeholder' "
-                                   "data-gif-id='%1' "
-                                   "data-mime-type='%2' "
-                                   "data-attachment-id='%3' "
-                                   "data-filename='%4'>"
-                                   "</div>")
-                               .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
-                                    attachment_mime_type,
-                                    attachment_id,
-                                    attachment_name);
+                                    QString("<div class='gif-placeholder' "
+                                        "data-gif-id='%1' "
+                                        "data-mime-type='%2' "
+                                        "data-attachment-id='%3' "
+                                        "data-filename='%4'>"
+                                        "</div>")
+                                    .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
+                                         attachment_mime_type,
+                                         attachment_id,
+                                         attachment_name);
             } else {
                 formatted_message = message_start + "<br>" +
-                               QString("<div class='image-placeholder' "
-                                   "data-image-id='%1' "
-                                   "data-mime-type='%2' "
-                                   "data-attachment-id='%3' "
-                                   "data-filename='%4'>"
-                                   "</div>")
-                               .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
-                                    attachment_mime_type,
-                                    attachment_id,
-                                    attachment_name);
+                                    QString("<div class='image-placeholder' "
+                                        "data-image-id='%1' "
+                                        "data-mime-type='%2' "
+                                        "data-attachment-id='%3' "
+                                        "data-filename='%4'>"
+                                        "</div>")
+                                    .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
+                                         attachment_mime_type,
+                                         attachment_id,
+                                         attachment_name);
             }
-        }
-        else if (attachment_type == "audio") {
+        } else if (attachment_type == "audio") {
             formatted_message = message_start + "<br>" +
-                           QString("<div class='audio-placeholder' "
-                               "data-audio-id='%1' "
-                               "data-mime-type='%2' "
-                               "data-attachment-id='%3' "
-                               "data-filename='%4'>"
-                               "</div>")
-                           .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
-                                attachment_mime_type,
-                                attachment_id,
-                                attachment_name);
-        }
-        else if (attachment_type == "video") {
+                                QString("<div class='audio-placeholder' "
+                                    "data-audio-id='%1' "
+                                    "data-mime-type='%2' "
+                                    "data-attachment-id='%3' "
+                                    "data-filename='%4'>"
+                                    "</div>")
+                                .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
+                                     attachment_mime_type,
+                                     attachment_id,
+                                     attachment_name);
+        } else if (attachment_type == "video") {
             formatted_message = message_start + "<br>" +
-                           QString("<div class='video-placeholder' "
-                               "data-video-id='%1' "
-                               "data-mime-type='%2' "
-                               "data-attachment-id='%3' "
-                               "data-filename='%4'>"
-                               "</div>")
-                           .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
-                                attachment_mime_type,
-                                attachment_id,
-                                attachment_name);
-        }
-        else {
+                                QString("<div class='video-placeholder' "
+                                    "data-video-id='%1' "
+                                    "data-mime-type='%2' "
+                                    "data-attachment-id='%3' "
+                                    "data-filename='%4'>"
+                                    "</div>")
+                                .arg(QUuid::createUuid().toString(QUuid::WithoutBraces),
+                                     attachment_mime_type,
+                                     attachment_id,
+                                     attachment_name);
+        } else {
             formatted_message = message_start + " " +
-                           QString("<a href='file:%1' style='color:#85c4ff;'>[%2]</a>")
-                           .arg(attachment_name, attachment_name);
+                                QString("<a href='file:%1' style='color:#85c4ff;'>[%2]</a>")
+                                .arg(attachment_name, attachment_name);
         }
     } else {
-        // Zwykła wiadomość tekstowa
         if (content.isEmpty()) {
             content = translator->Translate("MessageFormatter.EmptyMessage", "[Empty Message]");
         }
